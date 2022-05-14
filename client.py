e2e1c35d937b29625c457521267dc59fb4a04b26
Octavius <kosta_r96@mail.ru>
2022-05-14 19:22:22 +0600

    add_time-out_for_dns_client

--- client.py
+++ client.py
@@ -7,12 +7,10 @@ DNS_SERVER_IP = setting.get_param("DNS_SERVER_IP")
 
 def send_request(message):
     clien_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clien_sock.settimeout(60)
     try:
         clien_sock.sendto(message,(DNS_SERVER_IP,53))
         respons,_ = clien_sock.recvfrom(4096)
     finally:
         clien_sock.close()
     return respons
-
-
-
